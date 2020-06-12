6096f37a3b75622c3c7af22a2dae66087b1adf2b
Michael Griffin <michaelgriffin@sushidevelopers.tk>
2020-06-12 03:34:19 -0700

    Made GetKey() function work properly
    The return values were not defined in the header file, so the use of
    'if (GetKey() = PAD_DOWN)' would fail to compile due to an expression
    being expected.

--- blankModTemplate/pad.h
+++ blankModTemplate/pad.h
@@ -1,20 +1,20 @@
 #pragma once
 
-#define PAD_LEFT
-#define PAD_DOWN
-#define PAD_RIGHT
-#define PAD_UP
-#define PAD_START
-#define PAD_R3
-#define PAD_L3
-#define PAD_SELECT
-#define PAD_SQUARE
-#define PAD_CROSS
-#define PAD_CIRCLE
-#define PAD_TRIANGLE
-#define PAD_R1
-#define PAD_L1
-#define PAD_R2
-#define PAD_L2
+#define PAD_LEFT (1 << 7)
+#define PAD_DOWN (1 << 6)
+#define PAD_RIGHT (1 << 5)
+#define PAD_UP (1 << 4)
+#define PAD_START (1 << 3)
+#define PAD_R3 (1 << 2)
+#define PAD_L3 (1 << 1)
+#define PAD_SELECT (1 << 0)
+#define PAD_SQUARE (1 << 15)
+#define PAD_CROSS (1 << 14)
+#define PAD_CIRCLE (1 << 13)
+#define PAD_TRIANGLE (1 << 12)
+#define PAD_R1 (1 << 11)
+#define PAD_L1 (1 << 10)
+#define PAD_R2 (1 << 9)
+#define PAD_L2 (1 << 8)
 
 uint32_t GetKey(void);
